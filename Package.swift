80cb5ad8b0a5da1ff0bc7c58cb81a72dce58db83
Zhiyu Zhu/朱智语 <public-apollonian@outlook.com>
2019-03-27 19:30:20 -0400

    Swift 5 Update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,7 +19,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Carthage/Commandant.git", from: "0.15.0"),
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.4"),
-        .package(url: "https://github.com/ApolloZhu/srt.git", from: "1.0.1")
+        .package(url: "https://github.com/ApolloZhu/srt.git", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
